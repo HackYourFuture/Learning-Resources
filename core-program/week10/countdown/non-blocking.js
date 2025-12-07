ffef45d29033f04d8254b2940d448d9c4963ade1
Jim Cramer <remarcmij@gmail.com>
2025-12-07 10:07:33 +0100

    More examples for week 9

---
+++ core-program/week10/countdown/non-blocking.js
@@ -0,0 +1,52 @@
+let isRunning = false;
+
+function speak(text) {
+  const utterance = new SpeechSynthesisUtterance(text);
+  utterance.lang = 'en-US';
+  speechSynthesis.speak(utterance);
+}
+
+function timer(count) {
+  // Stop recursing if the timer should no longer run.
+  if (!isRunning) {
+    return;
+  }
+
+  console.log('count:', count);
+  document.querySelector('#counter').textContent = count;
+
+  // If the count reaches zero, we're done.
+  if (count === 0) {
+    isRunning = false;
+    speak('Countdown finished');
+    return;
+  }
+
+  // If the count is not zero, set a timeout to recursively call this function
+  // again after 1 second.
+  setTimeout(() => {
+    timer(count - 1);
+  }, 1000);
+}
+
+function startTimer(count) {
+  // If the timer is already running, do nothing.
+  if (isRunning) {
+    return;
+  }
+
+  // Indicate that the timer is running.
+  isRunning = true;
+
+  timer(count);
+}
+
+function stopTimer() {
+  // Cause the timer to stop running.
+  isRunning = false;
+}
+
+document
+  .querySelector('#start')
+  .addEventListener('click', () => startTimer(10));
+document.querySelector('#stop').addEventListener('click', stopTimer);
