db34fda7ffe62cef83c160b96045690823ab48cb
Jim Cramer <remarcmij@gmail.com>
2025-12-21 12:29:54 +0100

    Add custom promise example

---
+++ core-program/week-10/event-loop/promise-reject.js
@@ -0,0 +1,34 @@
+import { CustomPromise as Promise } from './lib/custom-promise.js';
+
+function whatIsTheMeaningOfLife() {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      () =>
+        reject(new Error('Come back in 7.5 million years and ask me again!')),
+      2000
+    );
+  });
+}
+
+function ask() {
+  let text = new Date().toISOString() + ' ';
+
+  return whatIsTheMeaningOfLife()
+    .then((result) => {
+      text += `The answer is ${result}`;
+    })
+    .catch((err) => {
+      text += `Error: ${err.message}`;
+    })
+    .then(() => {
+      console.log(text);
+      return Promise.resolve(); // fsPromises.appendFile('answers.log', text + '\n');
+    })
+    .catch((err) => {
+      console.error('Failed to write to log file:', err);
+    });
+}
+
+console.log('Hello!');
+ask();
+console.log('Goodbye!');
